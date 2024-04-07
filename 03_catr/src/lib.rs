ea98d595199d365fd01a5852765ff72ba881a046
junji.okuda <redgem@cameo.plala.or.jp>
2024-04-07 21:25:25 +0900

    3章まで

--- 03_catr/src/lib.rs
+++ 03_catr/src/lib.rs
@@ -1,4 +1,5 @@
-use clap::{App, Arg};
+use clap::Parser;
+// use clap::{Arg, App};
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -13,63 +14,96 @@ pub struct Config {
 }
 
 // --------------------------------------------------
-pub fn get_args() -> MyResult<Config> {
-    let matches = App::new("catr")
-        .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
-        .about("Rust cat")
-        .arg(
-            Arg::with_name("files")
-                .value_name("FILE")
-                .help("Input file(s)")
-                .multiple(true)
-                .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("number")
-                .short("n")
-                .long("number")
-                .help("Number lines")
-                .takes_value(false)
-                .conflicts_with("number_nonblank"),
-        )
-        .arg(
-            Arg::with_name("number_nonblank")
-                .short("b")
-                .long("number-nonblank")
-                .help("Number non-blank lines")
-                .takes_value(false),
-        )
-        .get_matches();
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    /// Input file(s)
+    #[arg(default_value = "-")]
+    files: Vec<String>,
 
+    /// Number lines
+    #[arg(short('n'), long, conflicts_with("number_nonblank"))]
+    number: bool,
+
+    /// Number non-blank lines
+    #[arg(short('b'), long("number-nonblank"))]
+    number_nonblank: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let args = Args::parse();
     Ok(Config {
-        files: matches.values_of_lossy("files").unwrap(),
-        number_lines: matches.is_present("number"),
-        number_nonblank_lines: matches.is_present("number_nonblank"),
+        files: args.files,
+        number_lines: args.number,
+        number_nonblank_lines: args.number_nonblank,
     })
+
+    // let matches = App::new("catr")
+    //     .version("0.1.0")
+    //     .author("Ken Youens-Clark <kyclark@gmail.com>")
+    //     .about("Rust cat")
+    //     .arg(
+    //         Arg::with_name("files")
+    //             .value_name("FILE")
+    //             .help("Input file(s)")
+    //             .multiple(true)
+    //             .default_value("-"),
+    //     )
+    //     .arg(
+    //         Arg::with_name("number")
+    //             .short("n")
+    //             .long("number")
+    //             .help("Number lines")
+    //             .takes_value(false)
+    //             .conflicts_with("number_nonblank"),
+    //     )
+    //     .arg(
+    //         Arg::with_name("number_nonblank")
+    //             .short("b")
+    //             .long("number-nonblank")
+    //             .help("Number non-blank lines")
+    //             .takes_value(false),
+    //     )
+    //     .get_matches();
+
+    // Ok(Config {
+    //     files: matches.values_of_lossy("files").unwrap(),
+    //     number_lines: matches.is_present("number"),
+    //     number_nonblank_lines: matches.is_present("number_nonblank"),
+    // })
 }
 
 // --------------------------------------------------
 pub fn run(config: Config) -> MyResult<()> {
+    let mut last_num: usize = 0;
     for filename in config.files {
         match open(&filename) {
             Err(e) => eprintln!("{}: {}", filename, e),
             Ok(file) => {
-                let mut last_num = 0;
-                for (line_num, line_result) in file.lines().enumerate() {
+                // let mut last_num = 0;
+                // for (line_num, line_result) in file.lines().enumerate() {
+                for line_result in file.lines() {
                     let line = line_result?;
-                    if config.number_lines {
-                        println!("{:6}\t{}", line_num + 1, line);
-                    } else if config.number_nonblank_lines {
-                        if !line.is_empty() {
-                            last_num += 1;
-                            println!("{:6}\t{}", last_num, line);
-                        } else {
-                            println!();
-                        }
+                    if config.number_lines || (config.number_nonblank_lines && !line.is_empty()) {
+                        last_num += 1;
+                        println!("{:6}\t{}", last_num, line);
                     } else {
-                        println!("{}", line);
+                        println!("{}", line)
                     }
+                    // if config.number_lines {
+                    //     last_num += 1;
+                    //     // println!("{:6}\t{}", line_num + 1, line);
+                    //     println!("{:6}\t{}", last_num, line);
+                    // } else if config.number_nonblank_lines {
+                    //     if !line.is_empty() {
+                    //         last_num += 1;
+                    //         println!("{:6}\t{}", last_num, line);
+                    //     } else {
+                    //         println!();
+                    //     }
+                    // } else {
+                    //     println!("{}", line);
+                    // }
                 }
             }
         }
