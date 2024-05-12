2d51192d2d6545c8b10d7bdced107c86c2b36cee
junji.okuda <redgem@cameo.plala.or.jp>
2024-05-12 15:30:36 +0900

    uniqrまで

--- 06_uniqr/src/lib.rs
+++ 06_uniqr/src/lib.rs
@@ -1,101 +1,76 @@
-use clap::{App, Arg};
+use clap::Parser;
+use anyhow::{Result, anyhow};
 use std::{
-    error::Error,
     fs::File,
     io::{self, BufRead, BufReader, Write},
 };
 
-type MyResult<T> = Result<T, Box<dyn Error>>;
-
-#[derive(Debug)]
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
 pub struct Config {
+    /// Input file
+    #[arg(value_name("IN_FILE"), default_value("-"))]
     in_file: String,
+
+    /// Output file
+    #[arg(value_name("OUT_FILE"))]
     out_file: Option<String>,
+
+    /// Show counts
+    #[arg(short('c'), long("count"))]
     count: bool,
 }
 
 // --------------------------------------------------
-pub fn get_args() -> MyResult<Config> {
-    let matches = App::new("uniqr")
-        .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
-        .about("Rust uniq")
-        .arg(
-            Arg::with_name("in_file")
-                .value_name("IN_FILE")
-                .help("Input file")
-                .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("out_file")
-                .value_name("OUT_FILE")
-                .help("Output file"),
-        )
-        .arg(
-            Arg::with_name("count")
-                .short("c")
-                .long("count")
-                .help("Show counts")
-                .takes_value(false),
-        )
-        .get_matches();
-
-    Ok(Config {
-        //in_file: matches.value_of_lossy("in_file").unwrap().to_string(),
-        //in_file: matches.value_of_lossy("in_file").map(String::from).unwrap(),
-        //in_file: matches.value_of_lossy("in_file").map(|v| v.into()).unwrap(),
-        in_file: matches.value_of_lossy("in_file").map(Into::into).unwrap(),
-        out_file: matches.value_of("out_file").map(|v| v.to_string()),
-        count: matches.is_present("count"),
-    })
+pub fn get_args() -> Result<Config> {
+    Ok(Config::parse())
 }
 
 // --------------------------------------------------
-pub fn run(config: Config) -> MyResult<()> {
-    let mut file = open(&config.in_file)
-        .map_err(|e| format!("{}: {}", config.in_file, e))?;
+pub fn run(config: Config) -> Result<()> {
+    let mut in_file = open(&config.in_file)
+        .map_err(|e| anyhow!("{}: {}", config.in_file, e))?;
 
     let mut out_file: Box<dyn Write> = match &config.out_file {
         Some(out_name) => Box::new(File::create(out_name)?),
         _ => Box::new(io::stdout()),
     };
 
-    let mut print = |count: u64, text: &str| -> MyResult<()> {
+    let mut print = |count: u64, text: &str| -> Result<()> {
         if count > 0 {
-            if config.count {
-                write!(out_file, "{:>4} {}", count, text)?;
-            } else {
-                write!(out_file, "{}", text)?;
-            }
+            let count_digits = match config.count {
+                true => format!("{:>4} ", count),
+                false => format!(""),
+            };
+            write!(out_file, "{}{}", count_digits, text)?;
         };
         Ok(())
     };
 
     let mut line = String::new();
-    let mut previous = String::new();
+    let mut prev = String::new();
     let mut count: u64 = 0;
     loop {
-        let bytes = file.read_line(&mut line)?;
+        line.clear();
+        let bytes = in_file.read_line(&mut line)?;
         if bytes == 0 {
             break;
         }
 
-        if line.trim_end() != previous.trim_end() {
-            print(count, &previous)?;
-            previous = line.clone();
+        if line.trim_end() != prev.trim_end() {
+            print(count, &prev)?;
+            prev = line.clone();
             count = 0;
         }
-
         count += 1;
-        line.clear();
     }
-    print(count, &previous)?;
+    print(count, &prev)?;
 
     Ok(())
 }
 
 // --------------------------------------------------
-fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
