a5cc67af6c29441894e8a4910f55528c43d088ac
junji.okuda <redgem@cameo.plala.or.jp>
2024-05-18 07:24:44 +0900

    findrまで

--- 07_findr/src/lib.rs
+++ 07_findr/src/lib.rs
@@ -1,114 +1,65 @@
-use crate::EntryType::*;
-use clap::{App, Arg};
+use clap::{builder::PossibleValue, Parser, ValueEnum};
+use anyhow::Result;
 use regex::Regex;
-use std::error::Error;
 use walkdir::{DirEntry, WalkDir};
 
-type MyResult<T> = Result<T, Box<dyn Error>>;
-
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum EntryType {
     Dir,
     File,
     Link,
 }
 
-#[derive(Debug)]
+impl ValueEnum for EntryType {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[EntryType::Dir, EntryType::File, EntryType::Link]
+    }
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(match self {
+            EntryType::Dir => PossibleValue::new("d"),
+            EntryType::File => PossibleValue::new("f"),
+            EntryType::Link => PossibleValue::new("l"),
+        })
+    }
+}
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
 pub struct Config {
+    /// Search path(s)
+    #[arg(value_name("PATH"), default_value("."))]
     paths: Vec<String>,
+
+    /// Name
+    #[arg(short('n'), long("name"), value_name("NAME"), value_parser(Regex::new), num_args(0..))]
     names: Vec<Regex>,
+
+    /// Entry type
+    #[arg(short('t'), long("type"), value_name("TYPE"), value_parser(clap::value_parser!(EntryType)), num_args(0..))]
     entry_types: Vec<EntryType>,
 }
 
 // --------------------------------------------------
-pub fn get_args() -> MyResult<Config> {
-    let matches = App::new("findr")
-        .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
-        .about("Rust find")
-        .arg(
-            Arg::with_name("paths")
-                .value_name("PATH")
-                .help("Search paths")
-                .default_value(".")
-                .multiple(true),
-        )
-        .arg(
-            Arg::with_name("names")
-                .value_name("NAME")
-                .short("n")
-                .long("name")
-                .help("Name")
-                .takes_value(true)
-                .multiple(true),
-        )
-        .arg(
-            Arg::with_name("types")
-                .value_name("TYPE")
-                .short("t")
-                .long("type")
-                .help("Entry type")
-                .possible_values(&["f", "d", "l"])
-                .multiple(true)
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let names = matches
-        .values_of_lossy("names")
-        .map(|vals| {
-            vals.into_iter()
-                .map(|name| {
-                    Regex::new(&name)
-                        .map_err(|_| format!("Invalid --name \"{}\"", name))
-                })
-                .collect::<Result<Vec<_>, _>>()
-        })
-        .transpose()?
-        .unwrap_or_default();
-
-    // clap should disallow anything but "d," "f," or "l"
-    let entry_types = matches
-        .values_of_lossy("types")
-        .map(|vals| {
-            vals.iter()
-                .map(|val| match val.as_str() {
-                    "d" => Dir,
-                    "f" => File,
-                    "l" => Link,
-                    _ => unreachable!("Invalid type"),
-                })
-                .collect()
-        })
-        .unwrap_or_default();
-
-    Ok(Config {
-        paths: matches.values_of_lossy("paths").unwrap(),
-        names,
-        entry_types,
-    })
+pub fn get_args() -> Result<Config> {
+    Ok(Config::parse())
 }
 
 // --------------------------------------------------
-pub fn run(config: Config) -> MyResult<()> {
+pub fn run(config: Config) -> Result<()> {
     let type_filter = |entry: &DirEntry| {
         config.entry_types.is_empty()
-            || config
-                .entry_types
-                .iter()
-                .any(|entry_type| match entry_type {
-                    Link => entry.file_type().is_symlink(),
-                    Dir => entry.file_type().is_dir(),
-                    File => entry.file_type().is_file(),
-                })
+        || config.entry_types.iter()
+            .any(|entry_type| match entry_type {
+                EntryType::Link => entry.file_type().is_symlink(),
+                EntryType::Dir => entry.file_type().is_dir(),
+                EntryType::File => entry.file_type().is_file(),
+            })
     };
 
     let name_filter = |entry: &DirEntry| {
         config.names.is_empty()
-            || config
-                .names
-                .iter()
-                .any(|re| re.is_match(&entry.file_name().to_string_lossy()))
+        || config.names.iter()
+            .any(|re| re.is_match(&entry.file_name().to_string_lossy()))
     };
 
     for path in &config.paths {
